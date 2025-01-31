78f087aa3c64d85d1c3fb80410e6ebce97546bbc
lasanga890 <lasangad890@gmail.com>
2025-01-31 14:50:18 +0530

    `Added login form with image banner and styling`

--- my-app/pages/Login.js
+++ my-app/pages/Login.js
@@ -1,8 +1,99 @@
+import { StyleSheet, Image, ScrollView, View } from "react-native";
+import { Button, Text, TextInput } from "react-native-paper";
+
 export default function Login() {
   return (
-    <div>
-      <h1>Login Page</h1>
-      {/* Add your login form here */}
-    </div>
+    <ScrollView>
+      <View>
+        <Image
+          source={{
+            uri: "https://vau.ac.lk/wp-content/uploads/2021/07/cropped-UoV_Logo.png",
+          }}
+          style={styles.banner}
+        />
+      </View>
+
+      <View>
+        <Text style={styles.heading}>Student Login</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          mode="outlined"
+          label={"username"}
+          style={styles.textInput}
+        />
+        <TextInput
+          mode="outlined"
+          label={"password"}
+          style={styles.textInput}
+        />
+        <Button mode="contained" style={styles.button}>
+          Login
+        </Button>
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  banner: {
+    width: 300,
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: 25,
+  },
+  inputContainer: {
+    alignItems: "center",
+    marginTop: 40,
+    width: "100%",
+  },
+  textInput: {
+    width: 300,
+    marginBottom: 20,
+    borderRadius: 10,
+  },
+  button: {
+    width: 300,
+    borderRadius: 20,
+    textAlign: "center",
+    marginTop: 20,
+  },
+  eyeIcon: {
+    position: "absolute",
+    right: 57,
+    top: 95,
+    color: "gray",
+  },
+  previewText: {
+    fontSize: 16,
+    marginTop: 10,
+    textAlign: "center",
+  },
+  errorContainer: {
+    backgroundColor: "pink",
+    width: 350,
+    height: 30,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 10,
+    flexDirection: "row",
+  },
+
+  errorContent: {
+    fontSize: 16,
+  },
+  infoIcon: {
+    fontSize: 18,
+    marginRight: 5,
+  },
+});
